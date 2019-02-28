72b7c9f85c16f487a927b213dd94c999e57294dd
KengoWada <louiskengo3@gmail.com>
2019-03-01 00:19:57 +0300

    Made 330,000 points

--- photos.py
+++ photos.py
@@ -13,7 +13,7 @@ def read(file_name):
         pic = {
             "orientation": x[0],
             "number_of_tags": int(x[2]),
-            "tags": list(x[4:-1].split(" "))
+            "tags": set(x[4:-1].split(" "))
         }
         pics[counter] = pic
         counter += 1
@@ -21,11 +21,51 @@ def read(file_name):
     return pics
 
 
-def commom_elements():
-    tags = read_file("filename")
-    for item in tags:
-        item = set(item)
+def create_slides():
+    pics =  read("filesin/e_shiny_selfies.txt")
+    slides = []
+    vertical = []
+    for pic, values in pics.items():
+        if values['orientation'] == 'H':
+            slides.append(pic)
+        else:
+            vertical.append(pic)
 
+    # sorted_list = []
+    # 
+    # for x in slides:
+    #     elements_x = len(pics[x]['tags'])
+    #     elements_x1 = len(pics[x + 1]['tags'])
+    #     intersection = len(pics[x]['tags'].intersection(pics[x + 1]['tags']))
+    #
+    #     min = min(elements_x-intersection, elements_x1-intersection, intersection)
+
+    if len(vertical) % 2 == 0:
+        vertical_pairs = [vertical[i:i+2] for i in range(0, len(vertical), 2)]
+    else:
+        vertical.pop()
+        vertical_pairs = [vertical[i:i+2] for i in range(0, len(vertical), 2)]
+
+    for v in vertical_pairs:
+        slides.append(v)
+
+    return slides
+
+def commom_elements():
+    slides = create_slides()
+
+    f = open('file_out/e_shiny_selfies.txt', 'w')
+
+    f.write(str(len(slides)) + '\n')
+
+    for slide in slides:
+        if type(slide) == int:
+            f.write(str(slide) + '\n')
+        else:
+            f.write(str(slide[0]) + " " + str(slide[1]) + '\n')
+
+    return "We are done"
 
 if __name__ == '__main__':
-    read("filesin/a_example.txt")
+    # create_slides()
+    commom_elements()
